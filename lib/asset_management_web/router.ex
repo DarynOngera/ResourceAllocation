a26f2c04a7a77d78a4da2bc404f49d7cf7f7c892
Ongera <ongeradaryn@gmail.com>
2026-01-28 12:22:27 +0300

    update router

--- lib/asset_management_web/router.ex
+++ lib/asset_management_web/router.ex
@@ -18,6 +18,7 @@ defmodule AssetManagementWeb.Router do
     pipe_through :browser
 
     get "/resources", ResourceController, :index
+    get "/users", UserController, :index
   end
 
   # Other scopes may use custom stacks.
